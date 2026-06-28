6c134fafa9a528aaba6df9195632479094392c99
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 14:35:38 +0530

    Add files via upload

---
+++ Module1/M1L2A2.py
@@ -0,0 +1,13 @@
+
+x = 89
+y = "Gyan"
+print(x)
+print(y)
+
+codingal = "codingal Technologies"
+print(codingal)
+
+
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
