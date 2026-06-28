723046961927d9d35f914c675c3ad003c0e78ac4
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 14:47:00 +0530

    Add files via upload

---
+++ Module1/M1L4A1.py
@@ -0,0 +1,14 @@
+
+tree1 = 789
+tree2 = 4978
+tree3 = 4145
+tree4 = 9508
+tree5 = 769
+
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum)
+
+
+average = sum/5
+print("the average of all the tree is :", average)
