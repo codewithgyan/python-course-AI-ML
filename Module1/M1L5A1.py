a925b7cf688b51138cf8d81a20b9a759eed618da
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 14:56:02 +0530

    Add files via upload

---
+++ Module1/M1L5A1.py
@@ -0,0 +1,8 @@
+num = 78
+if num > 0:
+    print(num, "is a positive number.")
+
+
+num = -56
+if num > 0:
+    print(num, "is a positive number.")
---
+++ Module1/M1L5A3.py
@@ -0,0 +1,7 @@
+i = int(input("enter a number : "))
+if (i < 17):
+    print (i," is smaller than 17")
+    print ("i'm in if Block")
+else:
+    print (i," is greater than 17")
+    print ("i'm in else Block")
