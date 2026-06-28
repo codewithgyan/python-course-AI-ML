37ad6929c18fbce05b7c7682b1565824b33c9f13
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 15:11:54 +0530

    Add files via upload

---
+++ Module1/M1L8A1.py
@@ -0,0 +1,17 @@
+v = 8
+w = 6
+x = 7
+y = 1
+z = 0
+z = (v+w) * x / y;   
+print("Value of (v+w) * x/ y is ",  z)
+
+
+
+name = "Gyan"
+age = 0
+  
+if name == "Gyan" or name == "Gyanendra" and age >= 2 : 
+  print("Hello! Welcome.")
+else :
+  print("Good Bye!!")
---
+++ Module1/M1L8A3.py
@@ -0,0 +1,15 @@
+mean1 = 78
+wrong_number=35
+correct_number=89
+total_number=69
+
+sum = mean1*total_number
+print("the sum of 69 number: ",sum)
+
+
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ",num2)
+
+
+mean2=num2/total_number
+print(mean2)
