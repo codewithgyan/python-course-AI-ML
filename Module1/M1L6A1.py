8e17216d28b938fe19041feb0563617a80546420
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 15:01:56 +0530

    Add files via upload

---
+++ Module1/M1L6A1.py
@@ -0,0 +1,24 @@
+a = 45
+b = 19
+c = 0
+  
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+
+
+
+a = 78
+b = -49
+c = 0
+  
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+  
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
