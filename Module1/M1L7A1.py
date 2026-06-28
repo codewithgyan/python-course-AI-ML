d8463eb9cad8d3d1c2a027f7c4872674dd82b831
Gyanendra Nath Pathak <82105895+codewithgyan@users.noreply.github.com>
2026-06-28 15:05:59 +0530

    Add files via upload

---
+++ Module1/M1L7A1.py
@@ -0,0 +1,39 @@
+
+
+
+x = 9
+
+if (type(x) is int):
+
+    print("true")
+
+else:
+
+    print("false")
+
+ 
+x = 17.99
+
+if (type(x) is not float):
+
+    print("true")
+
+else:
+
+    print("false")
+
+ 
+x = 45
+
+y = 45
+
+if (x is y):
+
+    print("x & y SAME identity")
+
+ 
+y = 90
+
+if (x is not y):
+
+    print("x & y have DIFFERENT identity")
